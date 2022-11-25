org-foundation/mirrors_sdl

commit c39e87bd0e9f7f7832c0ab7c5aa2152cbf97d6cf

removed M_PI detection from cmake, removed HAVE_M_PI from config files.
(didn't touch autofoo side for it so as not to break PR/6605)

include/SDL_config_windows.h
@@ -217,12 +217,6 @@ typedef unsigned int uintptr_t;
 #if _MSC_VER >= 1400
 #define HAVE__FSEEKI64 1
 #endif
-#ifdef _USE_MATH_DEFINES
-#define HAVE_M_PI 1
-#endif
-#else
-#define HAVE_M_PI 1
-#endif
 #else
 #define HAVE_STDARG_H   1
 #define HAVE_STDDEF_H   1

include/SDL_config_wingdk.h
@@ -155,11 +155,6 @@
 #define HAVE_TRUNC  1
 #define HAVE_TRUNCF 1
 #define HAVE__FSEEKI64 1
-#ifdef _USE_MATH_DEFINES
-#define HAVE_M_PI 1
-#endif
-#else
-#define HAVE_M_PI 1
 #endif
 #else
 #define HAVE_STDARG_H   1

include/SDL_config_xbox.h
@@ -155,11 +155,6 @@
 #define HAVE_TRUNC  1
 #define HAVE_TRUNCF 1
 #define HAVE__FSEEKI64 1
-#ifdef _USE_MATH_DEFINES
-#define HAVE_M_PI 1
-#endif
-#else
-#define HAVE_M_PI 1
 #endif
 #else
 #define HAVE_STDARG_H   1

CMakeLists.txt
@@ -996,7 +996,6 @@ if(SDL_LIBC)
       set(HAVE_${_UPPER} 1)
     endforeach()
     set(HAVE_ALLOCA 1)
-    check_symbol_exists(M_PI math.h HAVE_M_PI)
     set(STDC_HEADERS 1)
   else()
     set(HAVE_LIBC TRUE)
@@ -1032,7 +1031,6 @@ if(SDL_LIBC)
 
     set(STDC_HEADER_NAMES "stddef.h;stdarg.h;stdlib.h;string.h;stdio.h;wchar.h;float.h")
     check_include_files("${STDC_HEADER_NAMES}" STDC_HEADERS)
-    check_symbol_exists(M_PI math.h HAVE_M_PI)
     # TODO: refine the mprotect check
     check_c_source_compiles("#include <sys/types.h>
                              #include <sys/mman.h>

include/SDL_config.h.cmake
@@ -144,7 +144,6 @@
 #cmakedefine HAVE_SSCANF 1
 #cmakedefine HAVE_VSSCANF 1
 #cmakedefine HAVE_VSNPRINTF 1
-#cmakedefine HAVE_M_PI 1
 #cmakedefine HAVE_ACOS 1
 #cmakedefine HAVE_ACOSF 1
 #cmakedefine HAVE_ASIN 1

include/SDL_config_emscripten.h
@@ -104,7 +104,6 @@
 #define HAVE_SSCANF 1
 #define HAVE_VSSCANF 1
 #define HAVE_VSNPRINTF 1
-#define HAVE_M_PI 1
 #define HAVE_ACOS 1
 #define HAVE_ACOSF 1
 #define HAVE_ASIN 1

include/SDL_config_iphoneos.h
@@ -88,7 +88,6 @@
 #define HAVE_STRCASESTR 1
 #define HAVE_VSSCANF 1
 #define HAVE_VSNPRINTF  1
-#define HAVE_M_PI   1
 #define HAVE_ACOS   1
 #define HAVE_ACOSF  1
 #define HAVE_ASIN   1

include/SDL_config_macosx.h
@@ -91,7 +91,6 @@
 #define HAVE_STRCASESTR 1
 #define HAVE_VSSCANF 1
 #define HAVE_VSNPRINTF  1
-#define HAVE_M_PI   1
 #define HAVE_ACOS   1
 #define HAVE_ACOSF  1
 #define HAVE_ASIN   1

commit 8b88a01dbb9bd8e486e8dc3dd0ee0a7ea586add3

fixed SDL_config_windows.h (was broken by c39e87bd)

include/SDL_config_windows.h
@@ -217,6 +217,7 @@ typedef unsigned int uintptr_t;
 #if _MSC_VER >= 1400
 #define HAVE__FSEEKI64 1
 #endif
+#endif    /* _MSC_VER */
 #else
 #define HAVE_STDARG_H   1
 #define HAVE_STDDEF_H   1

include/SDL_config_wingdk.h
@@ -155,7 +155,7 @@
 #define HAVE_TRUNC  1
 #define HAVE_TRUNCF 1
 #define HAVE__FSEEKI64 1
-#endif
+#endif    /* _MSC_VER */
 #else
 #define HAVE_STDARG_H   1
 #define HAVE_STDDEF_H   1

include/SDL_config_xbox.h
@@ -155,7 +155,7 @@
 #define HAVE_TRUNC  1
 #define HAVE_TRUNCF 1
 #define HAVE__FSEEKI64 1
-#endif
+#endif    /* _MSC_VER */
 #else
 #define HAVE_STDARG_H   1
 #define HAVE_STDDEF_H   1