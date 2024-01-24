org-foundation/mirrors_sdl

commit 60146d88006be1bd5f0e1176492e71f519cc7e41

Define SDL_PLATFORM_* macros instead of underscored ones (#8875)

cmake/sdlchecks.cmake
@@ -849,7 +849,7 @@ macro(CheckPTHREAD)
         check_c_source_compiles("
             #include <pthread.h>
             int main(int argc, char **argv) {
-              #ifdef __APPLE__
+              #ifdef SDL_PLATFORM_APPLE
               pthread_setname_np(\"\");
               #else
               pthread_setname_np(pthread_self(),\"\");

commit 18947b96f7e88785ff9436001838a4f67d4ed00d

Don't try to use SDL platform macros in configure-time checks
At the point that we run this, nothing SDL-specific is set up yet.
__APPLE__ is a compiler predefined macro that forms part of the API on
Apple platforms, so it's fine to rely on it.

This partially reverts commit 60146d88.

Fixes: 60146d88 "Define SDL_PLATFORM_* macros instead of underscored ones (#8875)"
Signed-off-by: Simon McVittie <smcv@collabora.com>

cmake/sdlchecks.cmake
@@ -849,7 +849,7 @@ macro(CheckPTHREAD)
         check_c_source_compiles("
             #include <pthread.h>
             int main(int argc, char **argv) {
-              #ifdef SDL_PLATFORM_APPLE
+              #ifdef __APPLE__
               pthread_setname_np(\"\");
               #else
               pthread_setname_np(pthread_self(),\"\");