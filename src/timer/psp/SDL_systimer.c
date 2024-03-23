org-foundation/mirrors_sdl

commit c4f2f667464b859fa9a6dbf9049893bf93950a45

PSP: improve performance counter res to us granularity

src/timer/psp/SDL_systimer.c
@@ -30,35 +30,21 @@
 #include <time.h>
 #include <sys/time.h>
 #include <pspthreadman.h>
-
-static struct timeval start;
-static SDL_bool ticks_started = SDL_FALSE;
+#include <psprtc.h>
 
 void SDL_TicksInit(void)
 {
-    if (ticks_started) {
-        return;
-    }
-    ticks_started = SDL_TRUE;
-
-    gettimeofday(&start, NULL);
 }
 
 void SDL_TicksQuit(void)
 {
-    ticks_started = SDL_FALSE;
 }
 
 Uint64 SDL_GetTicks64(void)
 {
-    struct timeval now;
-
-    if (!ticks_started) {
-        SDL_TicksInit();
-    }
-
-    gettimeofday(&now, NULL);
-    return (Uint64)(((Sint64)(now.tv_sec - start.tv_sec) * 1000) + ((now.tv_usec - start.tv_usec) / 1000));
+    Uint64 ticks;
+    sceRtcGetCurrentTick(&ticks);
+    return ticks;
 }
 
 Uint64 SDL_GetPerformanceCounter(void)
@@ -68,7 +54,7 @@ Uint64 SDL_GetPerformanceCounter(void)
 
 Uint64 SDL_GetPerformanceFrequency(void)
 {
-    return 1000;
+    return sceRtcGetTickResolution();
 }
 
 void SDL_Delay(Uint32 ms)

commit 5092ca3c9b8a554bdccc5fc762515a0f2b90e6de

PSP: fix regression, GetTicks64 should return millisecs
introduced in c4f2f667464b859fa9a6dbf9049893bf93950a45

src/timer/psp/SDL_systimer.c
@@ -32,24 +32,35 @@
 #include <pspthreadman.h>
 #include <psprtc.h>
 
-void SDL_TicksInit(void)
-{
-}
+static Uint64 start_tick;
 
-void SDL_TicksQuit(void)
-{
-}
-
-Uint64 SDL_GetTicks64(void)
+static Uint64 PSP_Ticks(void)
 {
     Uint64 ticks;
     sceRtcGetCurrentTick(&ticks);
     return ticks;
 }
 
+void SDL_TicksInit(void)
+{
+    if (start_tick == 0) {
+        start_tick = PSP_Ticks();
+    }
+}
+
+void SDL_TicksQuit(void)
+{
+}
+
+/* return ticks as milliseconds */
+Uint64 SDL_GetTicks64(void)
+{
+    return (PSP_Ticks() - start_tick) / 1000ULL;
+}
+
 Uint64 SDL_GetPerformanceCounter(void)
 {
-    return SDL_GetTicks64();
+    return PSP_Ticks();
 }
 
 Uint64 SDL_GetPerformanceFrequency(void)