libsdl-org/SDL

X11: support _NET_WM_SYNC_REQUEST

cmake/sdlchecks.cmake
@@ -276,6 +276,7 @@ macro(CheckX11)
     find_file(HAVE_XINPUT2_H NAMES "X11/extensions/XInput2.h" HINTS "${X_INCLUDEDIR}")
     find_file(HAVE_XRANDR_H NAMES "X11/extensions/Xrandr.h" HINTS "${X_INCLUDEDIR}")
     find_file(HAVE_XFIXES_H_ NAMES "X11/extensions/Xfixes.h" HINTS "${X_INCLUDEDIR}")
+    find_file(HAVE_XSYNC_H NAMES "X11/extensions/sync.h" HINTS "${X_INCLUDEDIR}")
     find_file(HAVE_XRENDER_H NAMES "X11/extensions/Xrender.h" HINTS "${X_INCLUDEDIR}")
     find_file(HAVE_XSS_H NAMES "X11/extensions/scrnsaver.h" HINTS "${X_INCLUDEDIR}")
     find_file(HAVE_XSHAPE_H NAMES "X11/extensions/shape.h" HINTS "${X_INCLUDEDIR}")
@@ -417,6 +418,11 @@ macro(CheckX11)
         endif()
         set(SDL_VIDEO_DRIVER_X11_XFIXES 1)
         set(HAVE_X11_XFIXES TRUE)
+      endif()
+
+      if(SDL_X11_XSYNC AND HAVE_XSYNC_H AND XEXT_LIB)
+        set(SDL_VIDEO_DRIVER_X11_XSYNC 1)
+        set(HAVE_X11_XSYNC TRUE)
       endif()
 
       if(SDL_X11_XRANDR AND HAVE_XRANDR_H AND XRANDR_LIB)

org-foundation/mirrors_sdl

cmake/sdlchecks.cmake
@@ -307,6 +307,7 @@ macro(CheckX11)
     find_file(HAVE_XRANDR_H NAMES "X11/extensions/Xrandr.h" HINTS "${X11_INCLUDEDIR}")
     find_file(HAVE_XFIXES_H_ NAMES "X11/extensions/Xfixes.h" HINTS "${X11_INCLUDEDIR}")
     find_file(HAVE_XRENDER_H NAMES "X11/extensions/Xrender.h" HINTS "${X11_INCLUDEDIR}")
+    find_file(HAVE_XSYNC_H NAMES "X11/extensions/sync.h" HINTS "${X11_INCLUDEDIR}")
     find_file(HAVE_XSS_H NAMES "X11/extensions/scrnsaver.h" HINTS "${X11_INCLUDEDIR}")
     find_file(HAVE_XSHAPE_H NAMES "X11/extensions/shape.h" HINTS "${X11_INCLUDEDIR}")
     find_file(HAVE_XDBE_H NAMES "X11/extensions/Xdbe.h" HINTS "${X11_INCLUDEDIR}")
@@ -443,6 +444,11 @@ macro(CheckX11)
         endif()
         set(SDL_VIDEO_DRIVER_X11_XFIXES 1)
         set(HAVE_X11_XFIXES TRUE)
+      endif()
+
+      if(SDL_X11_XSYNC AND HAVE_XSYNC_H AND XEXT_LIB)
+        set(SDL_VIDEO_DRIVER_X11_XSYNC 1)
+        set(HAVE_X11_XSYNC TRUE)
       endif()
 
       if(SDL_X11_XRANDR AND HAVE_XRANDR_H AND XRANDR_LIB)