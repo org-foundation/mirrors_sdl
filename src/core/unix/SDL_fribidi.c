org-foundation/mirrors_sdl

commit d931efc00139dc07ae202e55b80e6a95543acf3b

unix: Mark SDL_FRIBIDI_DYNAMIC as a weak dependency
Signed-off-by: Simon McVittie <smcv@collabora.com>

src/core/unix/SDL_fribidi.c
@@ -25,6 +25,13 @@
 #include "SDL_fribidi.h"
 #include <fribidi.h>
 
+SDL_ELF_NOTE_DLOPEN(
+    "fribidi",
+    "Bidirectional text support",
+    SDL_ELF_NOTE_DLOPEN_PRIORITY_SUGGESTED,
+    SDL_FRIBIDI_DYNAMIC
+);
+
 SDL_FriBidi *SDL_FriBidi_Create(void) {
 	SDL_FriBidi *fribidi;
 

commit 42508f073cb5a462570845b1146febc5ebc9c408

Only emit dlopen note for SDL_FRIBIDI_DYNAMIC if not a hard dependency
Fixes: d931efc0 "unix: Mark SDL_FRIBIDI_DYNAMIC as a weak dependency"
Thanks: Anonymous Maarten

src/core/unix/SDL_fribidi.c
@@ -25,12 +25,14 @@
 #include "SDL_fribidi.h"
 #include <fribidi.h>
 
+#ifdef SDL_FRIBIDI_DYNAMIC
 SDL_ELF_NOTE_DLOPEN(
     "fribidi",
     "Bidirectional text support",
     SDL_ELF_NOTE_DLOPEN_PRIORITY_SUGGESTED,
     SDL_FRIBIDI_DYNAMIC
 );
+#endif
 
 SDL_FriBidi *SDL_FriBidi_Create(void) {
 	SDL_FriBidi *fribidi;