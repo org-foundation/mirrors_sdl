org-foundation/mirrors_sdl

commit 7b72aff6f51a5848c18aaec6dc9893ccd8748e17

Synchronize controller access on the Java side on Android

android-project/app/src/main/java/org/libsdl/app/SDLControllerManager.java
@@ -340,7 +340,7 @@ class SDLJoystickHandler {
         }
     }
 
-    synchronized protected SDLJoystick getJoystick(int device_id) {
+    protected SDLJoystick getJoystick(int device_id) {
         for (SDLJoystick joystick : mJoysticks) {
             if (joystick.device_id == device_id) {
                 return joystick;
@@ -354,7 +354,7 @@ class SDLJoystickHandler {
      * @param event the event to be handled.
      * @return if given event was processed.
      */
-    boolean handleMotionEvent(MotionEvent event) {
+    synchronized boolean handleMotionEvent(MotionEvent event) {
         int actionPointerIndex = event.getActionIndex();
         int action = event.getActionMasked();
         if (action == MotionEvent.ACTION_MOVE) {
@@ -524,7 +524,7 @@ class SDLJoystickHandler {
         return button_mask;
     }
 
-    void setLED(int device_id, int red, int green, int blue) {
+    synchronized void setLED(int device_id, int red, int green, int blue) {
         if (Build.VERSION.SDK_INT < 31 /* Android 12.0 (S) */) {
             return;
         }
@@ -542,7 +542,7 @@ class SDLJoystickHandler {
         joystick.lightsSession.requestLights(lightsRequest.build());
     }
 
-    void setSensorsEnabled(int device_id, boolean enabled) {
+    synchronized void setSensorsEnabled(int device_id, boolean enabled) {
         if (Build.VERSION.SDK_INT < 31 /* Android 12.0 (S) */) {
             return;
         }

commit 9ac28ee57621b00c9c07e127276053af0b981f9d

Fixed deadlock introduced by 7b72aff6f51a5848c18aaec6dc9893ccd8748e17
It turns out this change causes a deadlock:
The main UI thread calls synchronized handleMotionEvent() which then calls SDL_LockJoysticks()
The main app thread calls SDL_LockJoysticks() and then synchronized pollInputDevices()

android-project/app/src/main/java/org/libsdl/app/SDLControllerManager.java
@@ -346,7 +346,7 @@ class SDLJoystickHandler {
         }
     }
 
-    protected SDLJoystick getJoystick(int device_id) {
+    synchronized protected SDLJoystick getJoystick(int device_id) {
         for (SDLJoystick joystick : mJoysticks) {
             if (joystick.device_id == device_id) {
                 return joystick;
@@ -360,7 +360,7 @@ class SDLJoystickHandler {
      * @param event the event to be handled.
      * @return if given event was processed.
      */
-    synchronized boolean handleMotionEvent(MotionEvent event) {
+    boolean handleMotionEvent(MotionEvent event) {
         int actionPointerIndex = event.getActionIndex();
         int action = event.getActionMasked();
         if (action == MotionEvent.ACTION_MOVE) {
@@ -530,7 +530,7 @@ class SDLJoystickHandler {
         return button_mask;
     }
 
-    synchronized void setLED(int device_id, int red, int green, int blue) {
+    void setLED(int device_id, int red, int green, int blue) {
         if (Build.VERSION.SDK_INT < 31 /* Android 12.0 (S) */) {
             return;
         }
@@ -548,7 +548,7 @@ class SDLJoystickHandler {
         joystick.lightsSession.requestLights(lightsRequest.build());
     }
 
-    synchronized void setSensorsEnabled(int device_id, boolean enabled) {
+    void setSensorsEnabled(int device_id, boolean enabled) {
         if (Build.VERSION.SDK_INT < 31 /* Android 12.0 (S) */) {
             return;
         }